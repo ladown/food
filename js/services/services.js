c9a1ac26823d72a506ee51f14de2731533613d43
Egor Ermilov <egorka33rus52@gmail.com>
2020-11-06 21:09:22 +0300

    first_upload

---
+++ js/services/services.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data,
+    });
+
+    return await res.json();
+};
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Couldnt fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+};
+
+export {postData};
+export {getResource};
